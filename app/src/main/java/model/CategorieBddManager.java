98427c20aad7695fa42a97e1943a01c1a78cf26f
Axel legué <axel.legue31@gmail.com>
2016-11-28 11:24:03 +0100

    Création et insertion des données catégories dans la Base de donnée et externalisation des methodes dans BDDmanager

--- app/src/main/java/model/CategorieBddManager.java
+++ app/src/main/java/model/CategorieBddManager.java
@@ -2,6 +2,10 @@ package model;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import greendao.Categorie;
 import greendao.CategorieDao;
@@ -13,23 +17,49 @@ import greendao.DaoSession;
  */
 
 public class CategorieBddManager {
+    List<String> mResult = new ArrayList<>();
+
     public void insertCategorie(Context context, Categorie categorie) {
         DaoMaster.DevOpenHelper helper = null;
-
-
-
     }
 
+    // Récupère la liste des catégories présente en BDD
+    public void generateResultCategories(CategorieDao categorieDao) {
+        List<Categorie> categoriesList = getFromSQLCategories(categorieDao);
+        int size = categoriesList.size();
 
-    public CategorieDao setupDB(Context context){
-        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "appli.db", null);
-        SQLiteDatabase db = helper.getWritableDatabase();
-        DaoMaster master= new DaoMaster(db);
-        DaoSession masterSession = master.newSession();
+        if (size > 0) {
+            mResult.clear();
+            for (int i = 0; i < size; i++) {
+                Categorie currentItem = categoriesList.get(i);
+                mResult.add(0, currentItem.getId() + "," + currentItem.getNom() + "," + currentItem.getCouleur());
+                Log.v("test1", currentItem.getId() + "," + currentItem.getNom() + "," + currentItem.getCouleur());
+            }
+        }
+    }
+
+    // --------------------------------------  DB Setup Function ------------------------------------------- //
+    //Return the Configured LogDao Object
+    public CategorieDao setupDB(Context context) {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "appli.db", null);//Création de la base de donnée fichier db si il existe pas
+        SQLiteDatabase db = helper.getWritableDatabase(); // récupère  la base de donnée crée
+        DaoMaster master = new DaoMaster(db); // creation de de masterDao
+        DaoSession masterSession = master.newSession(); // création de la Session session
         return masterSession.getCategorieDao();
     }
 
-    public void SaveToSQL (Categorie categorie){
+    // --------------------------------------- '''' END DB Setup Function '''' --------------------- --- //
 
+    // ------------------------------------ '' SQL QUERY Fnctions ''' -------------------------------- //
+
+    public List<Categorie> getFromSQLCategories(CategorieDao categorieDao) {
+        List<Categorie> categories = categorieDao.queryBuilder().orderDesc(CategorieDao.Properties.Id).build().list();
+        return categories;
     }
+
+    public void SaveToSQLCategorie(CategorieDao categorieDao, Categorie categorie) {
+        categorieDao.insert(categorie);
+    }
+
+    // --------------------------------- ''' END SQL QUERY ''' -----------------------------------//
 }
