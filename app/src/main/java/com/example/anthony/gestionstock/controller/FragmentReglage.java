2770a709129624c3819de46de73f28ec09848d7a
Axel legué <axel.legue31@gmail.com>
2016-11-30 14:19:54 +0100

    Adapateur produit fragment reglage

--- app/src/main/java/com/example/anthony/gestionstock/controller/FragmentReglage.java
+++ app/src/main/java/com/example/anthony/gestionstock/controller/FragmentReglage.java
@@ -114,6 +114,7 @@ public class FragmentReglage extends Fragment implements View.OnClickListener, C
                 Toast.makeText(getContext(), "Add Categorie", Toast.LENGTH_SHORT).show();
                 DialogFragment newFragment = new DialogCategorie();
                 newFragment.show(getFragmentManager(), tag);
+
             }
         });
 
