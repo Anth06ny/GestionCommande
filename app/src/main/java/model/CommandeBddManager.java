ede318ab2f41639aca5712127a35019302766778
Anthony <anth06ny@gmail.com>
2016-12-09 14:39:54 +0100

    Ajout de la méthode getCommandeBetween A tester

--- app/src/main/java/model/CommandeBddManager.java
+++ app/src/main/java/model/CommandeBddManager.java
@@ -2,9 +2,12 @@ package model;
 
 import com.example.anthony.gestionstock.controller.MyApplication;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import greendao.Commande;
+import greendao.CommandeDao;
 
 /**
  * Created by Axel legué on 07/12/2016.
@@ -16,6 +19,23 @@ public class CommandeBddManager {
 
     public static List<Commande> getCommande() {
         return MyApplication.getDaoSession().getCommandeDao().loadAll();
+    }
+
+    public static List<Commande> getCommandeBetweenDate(Date debut, Date fin) {
+        Calendar cDebut = Calendar.getInstance();
+        cDebut.setTime(debut);
+        cDebut.set(Calendar.HOUR_OF_DAY, 0);
+        cDebut.set(Calendar.MINUTE, 0);
+        cDebut.set(Calendar.SECOND, 0);
+
+        Calendar cFin = Calendar.getInstance();
+        cFin.setTime(fin);
+        cFin.set(Calendar.HOUR_OF_DAY, 23);
+        cFin.set(Calendar.MINUTE, 59);
+        cFin.set(Calendar.SECOND, 59);
+
+        return MyApplication.getDaoSession().getCommandeDao().queryBuilder()
+                .where(CommandeDao.Properties.Date.between(cDebut.getTime(), cFin.getTime())).list();
     }
 
     public static void insertOrUpdate(Commande commande) {
