76e2731d74dc258e57d226ce5e6d8fda862839ea
Anthony <anth06ny@gmail.com>
2016-12-13 16:47:49 +0100

    Ajout d'une couleur de bg differente si la cellule est séléctionnée

--- app/src/main/java/vue/CategoryAdapter.java
+++ app/src/main/java/vue/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package vue;
 
 import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -41,13 +42,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         holder.displayModifyCategory.setVisibility(View.INVISIBLE);
         holder.displayDelete.setVisibility(View.INVISIBLE);
 
-        if (!categoriebean.isSelected()) {
-            holder.displayModifyCategory.setVisibility(View.INVISIBLE);
-            holder.displayDelete.setVisibility(View.INVISIBLE);
-        }
-        else {
+        if (categoriebean.isSelected()) {
             holder.displayModifyCategory.setVisibility(View.VISIBLE);
             holder.displayDelete.setVisibility(View.VISIBLE);
+            holder.cv_bg.setCardBackgroundColor(holder.cv_bg.getResources().getColor(R.color.selected_cellule_bg));
+        }
+        else {
+            holder.displayModifyCategory.setVisibility(View.INVISIBLE);
+            holder.displayDelete.setVisibility(View.INVISIBLE);
+            holder.cv_bg.setCardBackgroundColor(holder.cv_bg.getResources().getColor(R.color.unselected_cellule_bg));
         }
         holder.root.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +87,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         public AppCompatButton displayModifyCategory;
         public ImageView displayDelete;
         public View root;
+        public CardView cv_bg;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -92,6 +96,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
             displayModifyCategory = (AppCompatButton) itemView.findViewById(R.id.btn_modifiy_cat);
             displayDelete = (ImageView) itemView.findViewById(R.id.img_cat);
             root = itemView.findViewById(R.id.root);
+            cv_bg = (CardView) itemView.findViewById(R.id.cv_bg);
 
             displayDelete.setColorFilter(displayDelete.getResources().getColor(R.color.red));
         }
