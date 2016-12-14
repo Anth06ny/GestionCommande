8f0a9623ab4ea8b6bce799fc919eb5d64105c94f
Axel legué <axel.legue31@gmail.com>
2016-12-14 15:06:03 +0100

    Gestion admin page réglage terminer

--- app/src/main/java/com/example/anthony/gestionstock/controller/UserSession.java
+++ app/src/main/java/com/example/anthony/gestionstock/controller/UserSession.java
@@ -23,19 +23,27 @@ public class UserSession {
     public static final String KEY_NAME = "Name";
     public static final String KEY_PASSWORD = "MotDePasse";
 
-    private static final String USER_LOGIN = "Ad";
-    private static final String USER_PASSWORD = "1234";
+    public static final String USER_LOGIN = "Ad";
+    public static final String USER_PASSWORD = "1234";
 
     public UserSession(Context context) {
         this.context = context;
         preferences = context.getSharedPreferences(PREFER_NAME, PRIVATE_MODE);
         editor = preferences.edit();
-    }
-
-    public void createUserLoginSession(String uName, String uPsw) {
-        editor.putBoolean(IS_USER_LOGIN, true);
         editor.putString(KEY_NAME, USER_LOGIN);
         editor.putString(KEY_PASSWORD, USER_PASSWORD);
+        editor.commit();
+    }
+
+    //Create login session
+    public void createUserLoginSession(String uName, String uPsw) {
+        // Storing login value as TRUE
+        editor.putBoolean(IS_USER_LOGIN, true);
+        // Storing name in preferences
+        editor.putString(KEY_NAME, uName);
+        // Storing password in preferences
+        editor.putString(KEY_PASSWORD, uPsw);
+        // commit changes
         editor.commit();
     }
 
@@ -52,14 +60,22 @@ public class UserSession {
         return false;
     }
 
+    /**
+     * Get stored session data
+     */
     public HashMap<String, String> getUserDetails() {
         HashMap<String, String> user = new HashMap<>();
-
+        // user name
         user.put(KEY_NAME, preferences.getString(KEY_NAME, null));
+        // user password id
         user.put(KEY_PASSWORD, preferences.getString(KEY_PASSWORD, null));
+        // return user
         return user;
     }
 
+    /**
+     * Clear session details
+     */
     public void logoutUser() {
         editor.clear();
         editor.commit();
@@ -75,6 +91,7 @@ public class UserSession {
         context.startActivity(i);
     }
 
+    // Check for login
     public boolean isUserLoggedIn() {
         return preferences.getBoolean(IS_USER_LOGIN, false);
     }
